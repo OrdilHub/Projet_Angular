e504739ce55084b8140c5b9708342023139cca00
Ordilhub <tikollen6@gmail.com>
2021-01-27 13:27:23 +0100

    more stuff

--- src/app/edition/eleve/edition-routing.module.ts
+++ src/app/edition/eleve/edition-routing.module.ts
@@ -1,8 +1,15 @@
 import {NgModule} from '@angular/core'
 
 import {Routes, RouterModule} from '@angular/router'
+import { AccueilComponent } from 'src/app/accueil/accueil.component'
+import { EleveComponent } from 'src/app/eleve/eleve.component'
 
-export const routes= []
+export const routes= [
+    {path:'',component:AccueilComponent,children:[
+        {path:'etudiant',component:EleveComponent}
+        
+    ]}
+]
 
 @NgModule ({
     imports: [RouterModule.forRoot(routes)],
