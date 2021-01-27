5ee2ea89ecfce32354b809af60138c3a2c4ee06c
Ordilhub <tikollen6@gmail.com>
2021-01-27 10:42:25 +0100

    routing pour éditer eleve

---
+++ src/app/edition/eleve-routing.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core'
+
+import {Routes, RouterModule} from '@angular/router'
+
+export const routes= []
+
+@NgModule ({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class EleveRoutingModule{}
